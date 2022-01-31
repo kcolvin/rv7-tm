c3933377bbb4f74f89774a8c0fb93a16d2ec578e
Kurt Colvin <kcolvin@calpoly.edu>
2022-01-31 08:57:55 -0800

    Updated receive and send and added testdata.txt

--- receiveUDP/receive.py
+++ receiveUDP/receive.py
@@ -1,22 +1,25 @@
-# Main infinite loop program to ingest UDP messages from data radio.
+# Main infinite loop program to ingest UDP messages from RV-7 data radio.
 # The data messages arrive via a UDP message on ports 20003 and 20004
-# It receives 3 seprate messages:
-# On port 20003, AAHRS and EMS, alternating every .0625 seconds
-# On port 20004, GPS every 1 second
-# This program records the data in 2 different locations:
-#   - Raw data into a local .txt file with a filename built from the datetime stamp when it starts
-#   - Updates a single entry in a DynamoDB table to be used by the RemoteServer API call
+# You can read about UDP communications here:
+#           https://wiki.python.org/moin/UdpCommunication
+#
+# The Dynon sends 3 seprate messages over the radio:
+# On port 20003, ADAHRS and EMS, alternating every .0625 seconds. They look like this:
+#      ADAHRS: !1122054814+019+00051230000+00306-003+01+1099+002+180000230+00739XXXXX22
+#      EMS: !3222054815069+04011231123138029029060067069094144XXX+1020506004721XXXXXXXX....
+# On port 20004, GPS about every 1 second. Those lines look like this:
+#      GPS: $GPRMC,220527.00,A,3514.308512,N,12038.724165,W,9.5,124.7,301221,14.4,E,A*1C
+#
+# You can decode these lines using the 'DynonDataFormat.pdf' file from the github repo
+#
+# This program records the data in a raw format into a local .txt file
+# with a filename built from the datetime stamp when it starts.
 # 
 import socket
 import datetime
 import time
-import mysql.connector
-import boto3
 import select
-# import my own functions from myFunctions.py
-from parse_adahrs import parse_adahrs
-from myFunctions import parse_ems, build_query, create_payload, inject_to_stream
-
+#
 # Setup Data UDP socket to listen to port 20003 from anyhost
 UDP_PORT = 20003
 data_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
@@ -25,39 +28,16 @@ data_sock.bind(('',UDP_PORT))
 UDP_PORT = 20004
 gps_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
 gps_sock.bind(('',UDP_PORT))
-
-# Initialize Global Variables for DynamoDB table
-aircraft = 'N675CP'
-agl = 0.0
-com1 = 124.0
-com1standby = 120.6
-flaps = 0.0
-gspd = 0.0
-ias = 0.0
-lat = 35.0
-lon = -120.0
-magvar = 14.4
-mh = 290
-palt = 220
-pitch = -3.6
-roll = -4.2
-vs = 80
-winddir = 270
-windspeed = 10
-
+#
 # Initialize file to record raw data
 filename = time.strftime("%Y-%m-%d-%H%M%S"+".txt")
+print('Filename:',filename)
 f = open(filename, "x")
 #
-# Configure AWS using the right profile
-session = boto3.Session(profile_name='kcolvin-ime')
-# Setup  DynamoDB connection
-db_resource = session.resource('dynamodb', region_name='us-west-2')
-table = db_resource.Table('telemetry')
-
 # For terminal window:
 print('Init complete, listening....')
-
+print('Press CTRL-Break keys to exit.')
+#
 # infinite loop to process incoming UDP messages
 while True:
     # Wait for next Dynon message, then process it
@@ -65,23 +45,18 @@ while True:
     adahrs_raw = bytes.decode(data)
     # Write raw data to file
     f.write(adahrs_raw)
+    # Acknowledge write to file for testing purposes
+    print('Wrote to file:',adahrs_raw)
     # Check for valid ADAHRS message ('!1')
     if (adahrs_raw[1] == '1'):
-        # This is ADAHRS data, process it first
-        # Call parse function for ADAHRS
-        adahrs_lst = parse_adahrs(adahrs_raw)
-        #
         # Now get the next msg, which will be an EMS msg
         data, addr = data_sock.recvfrom(1024) # buffer size is 1024 bytes
         ems_raw = bytes.decode(data)
         # Write raw data to file
         f.write(ems_raw)
-        # Check for valid EMS message ('!3')
-        if (ems_raw[1] == '3'):
-            # Call parse fuction for EMS (don't really need this)
-            ems_lst = parse_ems(ems_raw)
-            # Print to screen
-            # print(ems_lst)
+        # Acknowledge write to file for testing purposes
+        print('Wrote to file:',ems_raw)
+
     # This code quickly checks the GPS socket for data 
     new_gps_data, _, _ = select.select([gps_sock],[],[],.025) #.025 is the timeout (time to wait)
     # If there is data , then process it
@@ -90,6 +65,9 @@ while True:
         gps_raw = bytes.decode(data)
         # write the byte array to the data file
         f.write(gps_raw)
+        #
+        # Parse the gps message and break into independent variables. 
+        # This is only as an example of how to parse the incoming data.
         # Convert the raw data into a list of values
         gps_lst = gps_raw.split(",")
         # This is the NMEA RMC sentence: https://www.trimble.com/OEM_ReceiverHelp/V4.44/en/NMEA-0183messages_RMC.html 
@@ -103,16 +81,5 @@ while True:
         gspd = float(gps_lst[7]) # ground track in kts
         #gtrk = float(gps_lst[8]) # ground track in degrees (not needed)
         magvar = float(gps_lst[10]) # magnetic variation in degrees
-        # Print GPS data to terminal screen
-        print('GPS lat:',lat,'lon:',lon,'gspd',gspd,'magvar',magvar)        
-        #
-        # This code will update the row in the DynamoDB table
-        # Build the payload, which is a Python dict
-        payload = create_payload(adahrs_lst,lat,lon,gspd,magvar)
-        print('Returned payload:', payload)
-        #
-        # Insert into DynamoDB
-        dynamo_response = table.put_item(Item = payload)
-        #print('DynamoDB Success: ',dynamo_response['ResponseMetadata']['RequestId'])
-
-
+        # Print GPS data to terminal screen for testing purposes
+        print('GPS lat:',lat,'lon:',lon,'gspd',gspd,'magvar',magvar,'\n')
